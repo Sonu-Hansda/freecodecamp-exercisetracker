0a6d0b28ce6b106309058347758c38ebf413c025
Sonu-Hansda <sonukumarhansda61@gmmail.com>
2024-01-22 10:52:31 +0530

    updated : solution added

--- index.js
+++ index.js
@@ -1,76 +1,167 @@
-require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.use(cors());
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+require("dotenv").config();
+
+const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let userSchema = new mongoose.Schema({
-  username: String,
-  count: Number,
+const userSchema = new mongoose.Schema({
+  username: { type: String, require: true },
   log: [
     {
-      description: String,
+      description: { type: String, require: true },
+      duration: { type: Number, require: true },
+      date: Date,
     },
-    { durations: Number },
-    { date: String },
   ],
 });
 
 const User = mongoose.model("User", userSchema);
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", async (req, res) => {
-  let username = req.body.username;
-  let user = new User({ username: username, count: 0 });
+// Add user
+app
+  .route("/api/users")
+  .post(async (req, res) => {
+    const username = req.body.username;
 
+    try {
+      if (username.length > 0) {
+        // Check user doesn't exist
+        const fu = await User.findOne({ username });
+        if (fu) {
+          console.log("User already exists ", username);
+          return res.json({ username, _id: fu._id });
+        }
+
+        // Add user to DB
+        const u = await User.create({ username });
+        await u.save();
+        res.json({ username, _id: u._id });
+      } else {
+        console.error("Username parameter is empty!");
+        return res.json({ error: "Username parameter is empty!" });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: error.toString() });
+    }
+    // Get users list
+  })
+  .get(async (req, res) => {
+    try {
+      const users = await User.find({}).select("_id username");
+      res.json(users);
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: error.toString() });
+    }
+  });
+
+// Add exercise
+app.post("/api/users/:_id/exercises", async (req, res) => {
   try {
-    let data = await user.save();
-    res.json({ username: data.username, _id: data._id });
-  } catch (err) {
-    console.log(err);
+    const id = req.params._id;
+    const user = await User.findOne({ _id: id });
+
+    if (user) {
+      // Validate data
+      const date = req.body.date
+        ? new Date(req.body.date).toDateString()
+        : new Date().toDateString();
+      const description = req.body.description;
+      const duration = parseInt(req.body.duration);
+      if (!description || !duration) {
+        console.error("Missing input data!");
+        console.error(req.body);
+        return res.json({ error: "Missing input data!" });
+      }
+
+      // Save data to DB
+      const exercise = {
+        description,
+        duration,
+        date,
+      };
+      user.log.push(exercise);
+      await user.save();
+
+      // Show saved data to user
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description,
+        duration,
+        date,
+      });
+    } else {
+      console.error("User id not found ", id);
+      res.json({ error: "User id not found!" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: error.toString() });
   }
 });
 
-app.post("/api/users/:_id/exercises", async (req, res) => {
-  let id = req.params._id;
-  let description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date;
-  if (date === "") {
-    date = new Date().toDateString();
-  } else {
-    date = new Date(date).toDateString();
-  }
+// Get user log
+app.get("/api/users/:_id/logs", async (req, res) => {
   try {
-    let user = await User.findById(id);
-    user.count = user.count + 1;
-    user.log.push({
-      description: description,
-      duration: duration,
-      date: date,
-    });
-    await user.save();
-    res.json({
-      _id: user._id,
-      username: user.username,
-      date: date,
-      duration: duration,
-      description: description,
-    });
-  } catch (err) {
-    console.log(err);
+    const id = req.params._id;
+    if (id) {
+      const limit = parseInt(req.query.limit) || null;
+      const from = req.query.from || new Date(0).toDateString();
+      const to = req.query.to || new Date().toDateString();
+      const user = await User.findById(id, {
+        log: {
+          $filter: {
+            input: "$log",
+            as: "item",
+            cond: {
+              $and: [
+                { $gte: ["$$item.date", new Date(from)] },
+                { $lte: ["$$item.date", new Date(to)] },
+              ],
+            },
+            limit: limit,
+          },
+        },
+      });
+
+      if (user) {
+        res.json({
+          _id: id,
+          username: user.username,
+          count: user.log.length,
+          log: user.log.map((item) => ({
+            description: item.description,
+            duration: item.duration,
+            date: new Date(item.date).toDateString(),
+          })),
+        });
+      } else {
+        console.error("User not found ", id);
+        return res.json({ error: "User not found!" });
+      }
+    } else {
+      console.error("Must provide a user id!");
+      return res.json({ error: "Must provide a user id!" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: error.toString() });
   }
 });
 
